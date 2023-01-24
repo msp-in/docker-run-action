20dd353e1d8b17b66d3e215694abc3e18938b478
pavhov <pavlik1993.am@gmail.com>
2023-01-24 08:49:17 +0400

    -- hide process

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [ ! -z $INPUT_USERNAME ];
-then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
+if [ ! -z $INPUT_USERNAME ]; then
+  echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
-if [ ! -z $INPUT_DOCKER_NETWORK ];
-then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
+if [ ! -z $INPUT_DOCKER_NETWORK ]; then
+  INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
 echo ${INPUT_OPTIONS}
